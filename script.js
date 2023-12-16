851794dcb13bf1a7d696e5225e3c8455ffb985da
Jayden Wehr <jwehr5@stumail.jccc.edu>
2023-12-16 14:19:54 -0600

    added files

--- script.js
+++ script.js
@@ -3,14 +3,21 @@
 $('#home-page').show();
 $('#resume-page').hide();
 $('#education-page').hide();
+$('#skills-page').hide();
+$('#projects-page').hide();
+$('#work-page').hide();
+$('#contact-page').hide();
 
 
 $('#home-button').click(function(){
     
-
     $('#home-page').slideDown(500);
     $('#resume-page').slideUp(500);
     $('#education-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#work-page').slideUp(500);
+    $('#contact-page').slideUp(500);
 
 })
 
@@ -19,6 +26,10 @@ $('#resume-button').click(function(){
     $('#resume-page').slideDown(500);
     $('#home-page').slideUp(500);
     $('#education-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#work-page').slideUp(500);
+    $('#contact-page').slideUp(500);
     
 })
 
@@ -26,6 +37,58 @@ $('#education-button').click(function(){
 
     $('#education-page').slideDown(500);
     $('#home-page').slideUp(500);
+    $('#resume-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#work-page').slideUp(500);
+    $('#contact-page').slideUp(500);
+    
+})
+
+$('#skills-button').click(function(){
+
+    $('#skills-page').slideDown(500);
+    $('#home-page').slideUp(500);
+    $('#resume-page').slideUp(500);
+    $('#education-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#work-page').slideUp(500);
+    $('#contact-page').slideUp(500);
+    
+})
+
+$('#projects-button').click(function(){
+
+    $('#projects-page').slideDown(500);
+    $('#home-page').slideUp(500);
+    $('#resume-page').slideUp(500);
+    $('#education-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#work-page').slideUp(500);
+    $('#contact-page').slideUp(500);
+    
+})
+
+$('#work-button').click(function(){
+
+    $('#work-page').slideDown(500);
+    $('#home-page').slideUp(500);
+    $('#resume-page').slideUp(500);
+    $('#education-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#contact-page').slideUp(500);
+    
+})
+
+$('#contact-button').click(function(){
+
+    $('#contact-page').slideDown(500);
+    $('#education-page').slideUp(500);
+    $('#skills-page').slideUp(500);
+    $('#home-page').slideUp(500);
+    $('#projects-page').slideUp(500);
+    $('#work-page').slideUp(500);
     $('#resume-page').slideUp(500);
     
 })
